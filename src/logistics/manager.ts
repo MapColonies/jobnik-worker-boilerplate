6d9aa5511c9d9667a655be3d2e5b70f475825265
Schnitz <12687466+CptSchnitz@users.noreply.github.com>
2025-11-03 11:16:42 +0200

    feat: added metrics support using the jobnik sdk

--- src/logistics/manager.ts
+++ src/logistics/manager.ts
@@ -1,9 +1,12 @@
+import { setTimeout as sleep } from 'node:timers/promises';
 import type { Task, TaskHandlerContext } from '@map-colonies/jobnik-sdk';
 import { injectable } from 'tsyringe';
 import type { LogisticJobTypes, LogisticStageTypes } from './types';
 
 async function pickupPackage(): Promise<void> {
-  return Promise.resolve();
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  const randomDelay = Math.floor(Math.random() * 120000);
+  await sleep(randomDelay);
 }
 
 @injectable()
@@ -12,7 +15,7 @@ export class LogisticsManager {
     task: Task<LogisticStageTypes['delivery']['task']>,
     context: TaskHandlerContext<LogisticJobTypes, LogisticStageTypes, 'hazmatTransport', 'delivery'>
   ): Promise<void> {
-    context.logger.info('Handling pickup task for quantity:', task.data.quantity);
+    context.logger.info({ msg: 'Handling pickup task for quantity:', quantity: task.data.quantity });
     const casualtyCount = context.job.userMetadata?.casualtyCount ?? 0;
     if (casualtyCount > task.data.quantity) {
       throw new Error('Casualty count exceeds quantity to be picked up');
